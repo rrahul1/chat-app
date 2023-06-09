00ebfdcb37bb2805bc2dbbcf75993753bd9b8141
Rahul Singh <rathore.rahul.1999@gmail.com>
2023-06-09 16:40:47 +0530

    firebase initialize

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAc4rCy4Ox7JISn3jGikCl_kOIftqIoNF0",
+  authDomain: "chat-web-app-8db81.firebaseapp.com",
+  databaseURL: "https://chat-web-app-8db81-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-8db81",
+  storageBucket: "chat-web-app-8db81.appspot.com",
+  messagingSenderId: "1028931193991",
+  appId: "1:1028931193991:web:b11ebf25d9cf3ea370bc22",
+};
+
+const app = firebase.initializeApp(config);
